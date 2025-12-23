c45081ac327936caa385f314534f492f1d0a0825
unknown <vlad.kharkovskiy10@gmail.com>
2025-12-23 17:30:31 +0200

    some fixes

--- client/src/api/client.js
+++ client/src/api/client.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const apiBaseUrl = import.meta.env.VITE_API_URL ?? 'http://localhost:3001/api';
+const apiBaseUrl = (import.meta.env.VITE_API_URL === 'http://server:3001/api') ? 'http://localhost:3001/api' : import.meta.env.VITE_API_URL ?? 'http://localhost:3001/api';
 
 const client = axios.create({
   baseURL: apiBaseUrl,
