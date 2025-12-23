0bf1252faf7ba61505a99990d467963092c87b68
unknown <vlad.kharkovskiy10@gmail.com>
2025-12-23 19:45:36 +0200

    add docker

--- client/src/api/tasks.js
+++ client/src/api/tasks.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3001/api/tasks';
+const API_URL = `${import.meta.env.VITE_API_URL}/tasks`;
 
 const tasksApi = {
     getAllTasks: async () => {
